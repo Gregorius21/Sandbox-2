ff23c69aca976c16a3a540135cb04707d389d2f2
Gregorius21 <72364117+Gregorius21@users.noreply.github.com>
2021-09-09 14:58:19 +0700

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <div class="main-wrapper">
+      <partials-navbar />
+      <partials-sidebar />
+
+      <!-- Main Content -->
+      <div class="main-content">
+        <section class="section">
+          <div class="section-header">
+            <h1>Blank Page</h1>
+            <!-- <img src="@/assets/img/example-image.jpg" alt=""> -->
+          </div>
+          <div class="section-body">
+            <Nuxt />
+          </div>
+        </section>
+      </div>
+      <partials-footer />
+    </div>
+  </div>
+</template>
+<script>
+import PartialsSidebar from "../components/partials/sidebar.vue";
+import PartialsNavbar from "../components/partials/navbar.vue";
+export default {
+  components: { PartialsSidebar, PartialsNavbar },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <center>
+      <NuxtLink to="/users"> User Nuxt LINK</NuxtLink> ||
+      <a href="/users">User A</a>
+    </center>
+    </template>

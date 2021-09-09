b819b456737f0a4b18bd97a282fa3f0595e1853a
Gregorius21 <72364117+Gregorius21@users.noreply.github.com>
2021-09-09 14:52:57 +0700

    Add files via upload

---
+++ components/class/item.vue
@@ -0,0 +1,70 @@
+<<<<<<< HEAD
+<template>
+  <div class="card">
+    <img :src="img" class="card-img-top" :alt="'foto ' +name">
+    <div class="card-body">
+      <h5 class="card-title">{{name}}</h5>
+      <p class="card-text">{{desc}}</p>
+      <button v-if="!unfollow" type="button" class="btn btn-primary" @click="followClass">Follow</button>
+      <button v-else type="button" class="btn btn-outline-danger" @click="followClass">Unfollow</button>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "Nama Kelas",
+    },
+    desc: {
+      type: String,
+      default: "Ini deskripsi kelasnya ya",
+    },
+    img: {
+      type: String,
+      default: "https://shiftacademy.id/wp-content/uploads/2021/09/1.png",
+    },
+    unfollow: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  methods: {
+    followClass() {
+      const data = {
+        name: this.name,
+        desc: this.desc,
+        img: this.img,
+      };
+      if (this.unfollow) this.$emit("unfollow-class", data);
+      else this.$emit("follow-class", data);
+    },
+  },
+};
+=======
+<template>
+  <div class="card">
+    <!-- <img src="..." class="card-img-top" alt="..."> -->
+    <div class="card-body">
+      <h5 class="card-title">{{name}}</h5>
+      <p class="card-text">{{desc}}</p>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      default: "Nama Kelas",
+    },
+    desc: {
+      type: String,
+      default: "Ini deskripsi kelasnya ya",
+    },
+  },
+};
+>>>>>>> 6d515e9e349a3f341e821dd44fe8affba6fa3280
+</script>
---
+++ components/input/type.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row my-2">
+    <div class="col-md-3">
+      <label class="text-capitalize" for="">{{name}}</label>
+    </div>
+    <div class="col-md-9">
+      <input type="text" v-model="val" class="form-control" :placeholder="'Masukkan '+name">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    name: { type: String, default: "input" },
+    value: { type: String, default: "" },
+  },
+  computed: {
+    val: {
+      get() {
+        return this.value;
+      },
+      set(value) {
+        this.$emit("get", value);
+      },
+    },
+  },
+};
+</script>
